9ade8ae3a26ca653eac0c5136110024f6dd7089f
Minsu <syt07100@gmail.com>
2022-04-27 00:24:13 +0900

    [환경설정]

---
+++ src/main/java/M/S/C/minsu/MinsuApplication.java
@@ -0,0 +1,13 @@
+package M.S.C.minsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinsuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinsuApplication.class, args);
+	}
+
+}
