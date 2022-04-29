f834a97120e5caa3636d3f023efb75263eb9acdb
Minsu <syt07100@gmail.com>
2022-04-29 15:37:53 +0900

    for test

---
+++ src/test/java/M/S/C/minsu/MinsuApplicationTests.java
@@ -0,0 +1,13 @@
+package M.S.C.minsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
