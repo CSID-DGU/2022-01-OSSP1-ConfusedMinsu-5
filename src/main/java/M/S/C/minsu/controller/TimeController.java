35e4c09c410989fcf75bf93985f8684172fdd087
parkmingyun99 <griffinland9@naver.com>
2022-05-19 20:11:21 +0900

    add_algo.md

---
+++ src/main/java/M/S/C/minsu/controller/TimeController.java
@@ -0,0 +1,16 @@
+package M.S.C.minsu.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Slf4j
+public class TimeController {
+
+
+
+
+}
