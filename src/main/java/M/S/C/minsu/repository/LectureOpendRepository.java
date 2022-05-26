3d347940f14b53c27f93f6cf51006479da7f368f
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2022-05-26 22:47:47 +0900

    create scheduleGuide 담기기능

--- src/main/java/M/S/C/minsu/repository/LectureOpendRepository.java
+++ src/main/java/M/S/C/minsu/repository/LectureOpendRepository.java
@@ -8,17 +8,23 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+import java.util.HashMap;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
+@RestController
 public class LectureOpendRepository {
-
+    private Map<String, Object> temp;
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     private RowMapper<LectureOpendDTO> lectureOpendMapper = BeanPropertyRowMapper.newInstance(LectureOpendDTO.class);
 
+    @PostMapping("/scheduleGuide")
     public List<LectureOpendDTO> getAllDataLectureOpend(){
 
         var query = "SELECT *" +
@@ -26,4 +32,31 @@ public class LectureOpendRepository {
 
         return jdbcTemplate.query(query, lectureOpendMapper);
     }
+
+    @PostMapping("/scheduleGuide/scheduleTable")
+    public Map<String,Object> makeTable(){
+        Map<String, Object> resultMap = new HashMap<String,Object>();
+        System.out.println("1"+temp);
+        return temp;
+    }
+
+
+    @RequestMapping(value="/scheduleTable.do", method=RequestMethod.POST)
+    public Map<String, Object> check(@RequestBody Map<String,Object> paramMap) throws SQLException, Exception{
+        System.out.println(paramMap);
+        Map<String, Object> resultMap = new HashMap<String,Object>();
+        temp = paramMap;
+
+        return resultMap;
+    }
+
+
+    /*@PostMapping("/scheduleGuide/{major}")
+    public List<LectureOpendDTO> getMajorDataLectureOpend(@PathVariable("major") String major){
+
+        var query = "SELECT *" +
+                " FROM Dongguk.LECTURE_OPEND WHERE lectureCode LIKE '\""+major+"\"@' ";
+
+        return jdbcTemplate.query(query, lectureOpendMapper);
+    }*/
 }
