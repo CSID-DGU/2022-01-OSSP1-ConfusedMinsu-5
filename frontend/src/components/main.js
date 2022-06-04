4409fcaca7fd88cb448c33bf8a92d28ab4e161df
JooYoon <dlwndbs10@gmail.com>
2022-06-05 01:05:15 +0900

    add graduateGuide Main page

--- frontend/src/components/main.js
+++ frontend/src/components/main.js
@@ -15,7 +15,7 @@ function Main() {
                   학과 정보를 알고싶은 당신! 학과별 교양강의, 전공강의, 졸업요건, 소모임을 알고싶다구요? 여기에요!
                 </Card.Text>
                 <Button variant="warning"><Link
-                    to={'/graduateGuide'}>확인하러가기</Link></Button>
+                    to={'/graduateGuide_main'}>확인 하러가기</Link></Button>
               </Card.Body>
             </Card>
             <br/>
@@ -27,7 +27,8 @@ function Main() {
                 <Card.Text>
                   학과 정보를 알고싶은 당신! 학과별 교양강의, 전공강의, 졸업요건, 소모임을 알고싶다구요? 여기에요!
                 </Card.Text>
-                <Button variant="warning">확인하러가기</Button>
+                <Button variant="warning"><Link
+                    to={'/graduateGuide_main'}>확인 하러가기</Link></Button>
               </Card.Body>
             </Card>
             <Card className="m-3" style={{width: '18rem'}}>
@@ -38,7 +39,8 @@ function Main() {
                 <Card.Text>
                   시간표 짜는게 복잡하죠? 우리랑 같이 짜봐요! 시간표만 입력하면 연강해도 가까운 거리로 짜볼게요!
                 </Card.Text>
-                <Button variant="warning">시간표보러가기</Button>
+                <Button variant="warning"><Link
+                    to={'/scheduleGuide'}>시간표 보러가기</Link></Button>
               </Card.Body>
             </Card>
             <Card className="m-3" style={{width: '18rem'}}>
@@ -49,7 +51,8 @@ function Main() {
                 <Card.Text>
                   공강일 때 시간이 붕 뜨는게 싫다면! 여기에 들어오세요! 건물별 빈강의실과 추천 카페리스트도 제공해요!
                 </Card.Text>
-                <Button variant="warning">시간 때우러가기</Button>
+                <Button variant="warning"><Link
+                    to={'/emptyGuide'}>시간 때우러가기</Link></Button>
               </Card.Body>
             </Card>
 
